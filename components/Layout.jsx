74b55816e1782f0e7e84cc0c084c5bc618387925
Ansh Chauhan <achauhan4580@gmail.com>
2022-03-30 16:13:25 +0530

    device transfer push

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,23 +1,13 @@
 /** @jsxImportSource theme-ui */
-import Navbar from "./Navbar"
-import Gradient from "./Gradient"
-import cssStyles from '../styles/Components.module.css'
 import { ThemeProvider } from "@theme-ui/core"
 import { theme } from "../utils/theme"
 
 export default function Layout ({children}) {
-    const styles = {
-        main: {
-            minHeight: "100vh",
-        }
-    }
     return (
         <>
             <ThemeProvider theme={theme}>
-                <div style={styles.main} sx={{ backgroundColor: "background" }}>
-                    <Gradient />
-                    <Navbar/>
-                    <div style={{ zIndex: 10, position: "relative" }}>
+                <div sx={{ backgroundColor: "background" }}>
+                    <div>
                         {children}
                     </div>
                 </div>
