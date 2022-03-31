ccac5527a887cf452173b3e7d787d333ad19ae20
AnshC <63086888+AnshC@users.noreply.github.com>
2022-03-31 20:20:08 +0530

    restarted projected

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,17 +1,15 @@
 /** @jsxImportSource theme-ui */
-import { ThemeProvider } from "@theme-ui/core"
-import { theme } from "../utils/theme"
+import { ThemeProvider } from "@theme-ui/core";
+import { theme } from "../utils/theme";
 
-export default function Layout ({children}) {
-    return (
-        <>
-            <ThemeProvider theme={theme}>
-                <div sx={{ backgroundColor: "background" }}>
-                    <div>
-                        {children}
-                    </div>
-                </div>
-            </ThemeProvider>
-        </>
-    )
+export default function Layout({ children }) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <div sx={{ backgroundColor: "background" }}>
+          <div>{children}</div>
+        </div>
+      </ThemeProvider>
+    </>
+  );
 }
--- utils/theme.js
+++ utils/theme.js
@@ -1,16 +1,16 @@
 export const theme = {
-    colors: {
-        intialColorMode: "dark",
-        text: "#ffffff",
-        background: "#030027",
-        accent: "#92bcea",
-        backgroundInverse: "#ffffff",
-        modes: {
-            light: {
-                text: "#121217",
-                background: "#ffffff",
-                backgroundInverse: "#030027"
-            }
-        }
-    }
-}
+  colors: {
+    intialColorMode: "dark",
+    text: "#F8F8F8",
+    background: "#030027",
+    accent: "#75b7ff",
+    backgroundInverse: "#F8F8F8",
+    modes: {
+      light: {
+        text: "#121217",
+        background: "#F8F8F8",
+        backgroundInverse: "#030027",
+      },
+    },
+  },
+};
